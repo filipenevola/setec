0ed8cc2bb09ad3febe30f96edca996b0a63640b1
filipenevola <filipemkv@gmail.com>
2018-08-08 13:56:38 -0400

    ex06 - mostrando um filme por linha

--- ui/Movies.js
+++ ui/Movies.js
@@ -1,8 +1,11 @@
 import React from 'react';
 
-export const Movies = ({ movies, search }) => {
-  return movies
-    .filter(movie => movie.name.toLowerCase().includes(search.toLowerCase()))
-    .map(movie => movie.name)
-    .join(', ');
-};
+export const Movies = ({ movies, search }) => (
+  <div>
+    {movies
+      .filter(movie => movie.name.toLowerCase().includes(search.toLowerCase()))
+      .map(movie => (
+        <div key={movie.name}>{movie.name}</div>
+      ))}
+  </div>
+);
